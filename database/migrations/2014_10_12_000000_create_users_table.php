4124d1b9f59b448c5f75bb6d78b590d077444bfd
gustavo <gustavodevaler@gmail.com>
2021-02-14 23:35:37 -0300

    publicacion spatie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('adress');
+            $table->string('province');
+            $table->string('city');
+            $table->string('floor');
+            $table->string('apartment');
+            $table->string('phone')->nullable();
+            $table->string('cell_phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
